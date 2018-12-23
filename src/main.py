059adea331620b4801a028d419acd8e40983541c
Alex <alex.lx.zhang@gmail.com>
2018-12-22 21:13:12 -0500

    Plottable IMU points

--- src/main.py
+++ src/main.py
@@ -4,16 +4,39 @@
 from libs.mpu9250.mpu9250.mpu9250 import mpu9250
 from time import sleep
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 imu = mpu9250()
 
 def main():
-	try:
-		while True:
-			a = imu.accel
-			print("Accel" + a[0] + a[1] + a[2])
-			sleep(0.5)
-	except KeyboardInterrupt:
-		print("end")
+    ax, ay, az, gx, gy, gz = []
+
+    for i in range(50):
+        a = imu.accel
+        print("a {:.3f} {:.3f} {:.3f}".format(*a))
+        axt, ayt, azt = a
+        ax.append(axt)
+        ay.append(ayt)
+        az.append(azt)
+
+        b = imu.gyro
+        print("g {:.3f} {:.3f} {:.3f}".format(*b))
+        gxt, gyt, gzt = b
+        gx.append(gxt)
+        gy.append(gyt)
+        gz.append(gzt)
+
+        sleep(0.25)
+
+    plots = [ax, ay, az, gx, gy, gz]
+    plot(plots)
+
+def plot(plots):
+    for i in range(len(plots)):
+        plt.plot(plots[i])
+    plt.legend(['ax', 'ay', 'az', 'gx', 'gy', 'gz'], loc='upper left')
+    plt.show()
 
 if __name__ == "__main__":
-	main()
+    main()
